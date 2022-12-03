4033d7ca114e2c65f926de4fa9732a01c5a3731e
DKarandikar <daniel.karandikar@zoho.com>
2022-12-03 13:31:10 +0000

    Do day 2 part 1

--- src/day2.purs
+++ src/day2.purs
@@ -1,9 +1,26 @@
 module Day2 where
 
+import Data.String
+
+import Data.Foldable
+
 import Prelude
 
 exec2a:: String -> String
-exec2a s = s 
+exec2a s = show $ foldl fold2a 0 (split (Pattern ("\n")) s)
 
 exec2b:: String -> String
-exec2b s = s 
+exec2b s = s
+
+fold2a :: Int -> String -> Int
+fold2a x row = x + case row of 
+    "A X" -> 1 + 3
+    "A Y" -> 2 + 6
+    "A Z" -> 3 + 0
+    "B X" -> 1 + 0
+    "B Y" -> 2 + 3
+    "B Z" -> 3 + 6
+    "C X" -> 1 + 6
+    "C Y" -> 2 + 0
+    "C Z" -> 3 + 3
+    _ -> 0 
